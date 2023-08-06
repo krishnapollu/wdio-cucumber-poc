889dff34e08e4d69afa9e32b3727fdb6a44bf696
Krishna S <Krishna.Subrahmania@ibsplc.com>
2023-08-05 20:46:53 -0500

    fix

--- test/specs/Something.js
+++ test/specs/Something.js
@@ -2,7 +2,7 @@ const pg = require('../pageobjects/web/Google.page')
 module.exports = {
     async doSomething(data) {
 
-        console.log('data here is: ');
+        console.log('data here is : ');
         console.log(data);
         await pg.doSearch(data.keyword);
         await pg.verifySearchSuccess(data.keyword);
